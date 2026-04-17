62001055b8e6594b3f34982eb92664e6ce5d044e
draftsbymatthew <matthewjett17@gmail.com>
2026-04-18 04:21:10 +0800

    add designs in css file

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,26 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      
+
       <div className="container">
 
-          <h1>BMI Calculator</h1> 
+        <h1>BMI Calculator</h1>
 
-          {/* Height */}
-          <label htmlFor="height">Height (cm)</label>
-          <input type="number" id='height' placeholder="e.g. 170" />
+        {/* Height */}
+        <label htmlFor="height">Height (cm)</label>
+        <input type="number" id='height' placeholder="e.g. 170" />
 
-          {/* Weight */}
-          <label htmlFor="weight">Weight (kg)</label>
-          <input type="number" id='weight' placeholder="e.g. 65" />
+        {/* Weight */}
+        <label htmlFor="weight">Weight (kg)</label>
+        <input type="number" id='weight' placeholder="e.g. 65" />
 
-          {/* Button */}
-          <button>Calculate</button>
+        {/* Button */}
+        <button>Calculate</button>
 
-          {/* Result */}
-          <div className="result"></div>
+        {/* Result */}
+        <div className="result">This is a test for result</div>
+
+        {/*  */}
 
       </div>
 

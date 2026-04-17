e0c222abe206a2230785ca4c77572dd57531db1d
draftsbymatthew <matthewjett17@gmail.com>
2026-04-18 03:59:42 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      
+      <div className="container">
+
+          <h1>BMI Calculator</h1> 
+
+          {/* Height */}
+          <label htmlFor="height">Height (cm)</label>
+          <input type="number" id='height' placeholder="e.g. 170" />
+
+          {/* Weight */}
+          <label htmlFor="weight">Weight (kg)</label>
+          <input type="number" id='weight' placeholder="e.g. 65" />
+
+          {/* Button */}
+          <button>Calculate</button>
+
+          {/* Result */}
+          <div className="result"></div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App 

b7f3868a2285e51c71dd1d5819f76f0543755953
draftsbymatthew <matthewjett17@gmail.com>
2026-04-18 14:08:48 +0800

    Added function to the inputs and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React from 'react';
+import {useState} from 'react';
 
 const App = () => {
+
+    const [height, setHeight] = useState("");
+    const [weight, setWeight] = useState("");
+    const [result, setResult] = useState("");
+
+    const calculateBMI = () => {
+      if (!height || !weight){
+        setResult("Please enter both height and weight!");
+        return;
+      }
+
+      const heightInMeters = height / 100;
+      const bmi =(weight / (heightInMeters * heightInMeters)).toFixed(1);
+
+      let category = "";
+      if (bmi < 18.5) category = "Underweight";
+      else if (bmi < 25) category = "Normal Weight";
+      else if (bmi < 30)  category = "Overweight";
+      else category = "Obese";
+
+      setResult(`Your BMI is ${bmi} : You are ${category}`)
+    };
+
+
   return (
     <div>
 
@@ -10,17 +35,27 @@ const App = () => {
 
         {/* Height */}
         <label htmlFor="height">Height (cm)</label>
-        <input type="number" id='height' placeholder="e.g. 170" />
+        <input 
+          type="number"  
+          id='height' 
+          placeholder="e.g. 170"
+          value={height}
+          onChange={(e) => setHeight(e.target.value)} />
 
         {/* Weight */}
         <label htmlFor="weight">Weight (kg)</label>
-        <input type="number" id='weight' placeholder="e.g. 65" />
+        <input 
+          type="number" 
+          id='weight' 
+          placeholder="e.g. 65"
+          value={weight}
+          onChange = {(e) => setWeight(e.target.value)} />
 
         {/* Button */}
-        <button>Calculate</button>
+        <button onClick = {calculateBMI}>Calculate</button>
 
         {/* Result */}
-        <div className="result">This is a test for result</div>
+        <div className="result">{result}</div>
 
         {/*  */}
 
